c434571114f7d79f6db581d728858d3fd3c2aa80
Nathaniel Wise <nathaniel.wise@gmail.com>
2026-03-17 12:50:48 -0400

    nav menu updates

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('nathanieljwise.com Flask app loaded');
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("nathanieljwise.com Flask app loaded");
 });
+
+const activeLink = document.querySelector('.sidebar nav ul li a.active');
+if (activeLink) {
+    activeLink.scrollIntoView({ block: 'start', behavior: 'instant' });
+}
